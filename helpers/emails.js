af3feec92a6c1969a986a4e7511174d362213ce9
Mariano <dev.marianohayward@gmail.com>
2024-02-01 02:05:04 -0300

    fix email?

--- helpers/emails.js
+++ helpers/emails.js
@@ -1,33 +1,46 @@
 import nodemailer from "nodemailer";
 
 const emailRegistro = async (datos) => {
-  const transport = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-  const { email, nombre, token } = datos;
+  try {
+    const transport = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
 
-  // Enviar el email
-  await transport.sendMail({
-    from: "BienesRaices.com",
-    to: email,
-    subject: "Confirma tu cuenta en bienesraices.com",
-    text: "Confirma tu cuenta en bienesraices.com",
-    html: `
-    <p>Hola ${nombre}, comprueba tu cuenta en bienesraices.com</p>
-    <p>Tu cuenta ya está lista, solo debes confirmarla en el sigueinte enlace:
-    <a href="${process.env.BACKEND_URL}:${
-      process.env.PORT ?? 3000
-    }/auth/confirmar-cuenta/${token}">Confirmar cuenta<a/></p>
+    const { email, nombre, token } = datos;
 
-    <p>Si tu no creaste esta cuenta, puedes ignorar el mensaje</p>
+    // Enviar el email
+    await transport.sendMail({
+      from: "BienesRaices.com",
+      to: email,
+      subject: "Confirma tu cuenta en bienesraices.com",
+      text: "Confirma tu cuenta en bienesraices.com",
+      html: `
+        <p>Hola ${nombre}, comprueba tu cuenta en bienesraices.com</p>
+        <p>Tu cuenta ya está lista, solo debes confirmarla en el siguiente enlace:
+        <a href="${process.env.BACKEND_URL}:${
+        process.env.PORT ?? 3000
+      }/auth/confirmar-cuenta/${token}">Confirmar cuenta<a/></p>
 
-    `,
-  });
+        <p>Si tú no creaste esta cuenta, puedes ignorar el mensaje</p>
+      `,
+    });
+
+    console.log("Correo electrónico enviado con éxito.");
+  } catch (error) {
+    console.error("Error al enviar el correo:", error.message);
+  }
+};
+
+// Ejemplo de uso
+const datosEjemplo = {
+  email: "dev.marianohayward@gmail.com",
+  nombre: "Usuario Ejemplo",
+  token: "tokenEjemplo",
 };
 
 const emailOlvidePassword = async (datos) => {
